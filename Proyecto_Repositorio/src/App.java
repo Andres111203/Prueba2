96730414772c7b382622f81a5de4eb808a5145d0
Andres111203 <andres.canas@correounivalle.edu.co>
2024-03-08 15:26:55 -0500

    modificacion del la primera clase

--- Proyecto_Repositorio/src/App.java
+++ Proyecto_Repositorio/src/App.java
@@ -2,6 +2,9 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
         System.out.println("hola esto va para el repositorio");
+        
+        System.out.println("esta es una modificacion para el repositorio");
+
 
         
 
